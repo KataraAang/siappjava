6417995364524f4d1339e8a19b0a848eac408814
Tagrid <tagridottii@gmail.com>
2020-06-01 21:28:20 +0300

    added djena modules

---
+++ moduled/src/main/java/org/moduled/App.java
@@ -0,0 +1,13 @@
+package org.moduled;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ModuleD!" );
+    }
+}
